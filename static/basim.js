027c424c304d35e8724fbca7e0a2601f84175002
basim341 <119255663+basim341@users.noreply.github.com>
2022-11-30 00:47:53 +0500

    first commit

---
+++ static/basim.js
@@ -0,0 +1,45 @@
+
+var string1 = ' my'
+var string2 = ' name'
+var string4 = ' basim'
+let name1 = ` Ali`;
+let name2 = ` Anwer`;
+let name3 = ` basim` ;
+let name4 = ` Salahuddin`;
+let greeting1 = `Hi!` 
+let greeting2 = ` hope you are doing well welcome to the website :) `
+var string3 = ' is'
+console.log(string1 + string2 + string3 + string4)
+
+function welcomeline(name) {
+    console.log(greeting1 + name + greeting2)
+
+
+}
+welcomeline(name1)
+welcomeline(name2)
+welcomeline(name3)
+welcomeline(name4)
+
+alert(`Hi`)  
+
+
+
+function togglehide () {
+    let btn = document.getElementById (`hidebtn`)
+    let para = document.getElementById (`paragraph`)
+   if(paragraph.style.display != `none`  ) {
+    paragraph.style.display = `none`
+   }
+   else{    paragraph.style.display = `block`}
+}
+
+function surprise() {
+    
+    alert( `You Really Thought There Will Be A Surprise For You` )
+
+}
+
+let meth = Math; 
+console.log (meth)
+
